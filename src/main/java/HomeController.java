c0c231775356c488d7e381899bb3eb42935eab67
arlefebvre <arthur.lefebvre76@gmail.com>
2016-04-05 20:42:42 +0200

    Mise en place starter web Spring Boot

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Arthur on 05/04/2016.
+ */
+@Controller
+@EnableAutoConfiguration
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HomeController.class, args);
+    }
+}
