1cf9c8de8215b8850e1cff1198cef232950e5377
ALefebvre <alefebvre@isagri.fr>
2016-04-08 18:53:22 +0200

    UEFA teams controller

--- src/main/java/fr/arlefebvre/pronostics/model/Team.java
+++ src/main/java/fr/arlefebvre/pronostics/model/Team.java
@@ -4,14 +4,18 @@ import org.springframework.data.annotation.Id;
 
 /**
  * Created by alefebvre on 06/04/2016.
- *
+ * <p>
  * Represents a football team (club or nation)
  */
 public class Team {
     @Id
     private String id;
 
+    private String countryCode;
+
     private String name;
+
+    private String imgUrl;
 
     public String getName() {
         return name;
@@ -19,5 +23,21 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 }
--- src/main/resources/static/app/scripts/controllers.js
+++ src/main/resources/static/app/scripts/controllers.js
@@ -1,11 +1,15 @@
  (function(angular) {
- var TeamController =function TeamCtrl($scope, Team) {
+ //var TeamController =function ($scope) {
+ var TeamController =function ($scope, Team) {
+ //console.log(Team.query());
 $scope.teams = Team.query();
+//$scope.teams = [{ "name" : "France" }, { "name" : "Brazil" }];
 
 //$scope.select = function(team) {
 //    $scope.editedPhone = phone;
 //};
 }
+//TeamController.$inject = ['$scope'];
 TeamController.$inject = ['$scope', 'Team'];
 angular.module("myApp.controllers").controller("TeamController", TeamController);
 }(angular));
